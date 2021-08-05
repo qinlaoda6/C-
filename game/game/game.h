ecddac8ae8f39b72fde88c8f092c7e165a8d1b1c
qinlaodaZzz <88325534+qinlaoda6@users.noreply.github.com>
2021-08-06 00:01:50 +0800

    C语言 五子棋 玩家与电脑对战

---
+++ game/game/game.h
@@ -0,0 +1,10 @@
+#define ROW 3
+#define COL 3
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+void InitBoard(char board[ROW][COL],int row,int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char borad[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL],int row,int col);
+int IsWin(char board[ROW][COL], int row, int col);
